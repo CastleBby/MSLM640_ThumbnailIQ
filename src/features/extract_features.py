d2fa522f3147d6f73e6e24c63fc16b9c53a7f0ae
Emily Castelan <emilycastelan@Emilys-Air.hsd1.ca.comcast.net>
2026-03-30 01:13:42 -0400

    reqs text updated and extract_features.py started

---
+++ src/features/extract_features.py
@@ -0,0 +1,24 @@
+""" 
+src/features/extract_features.py
+
+Usage: 
+------------
+
+
+
+Data Collected:
+---------------
+
+
+Methodology:
+------------
+
+
+Inputs:
+------------
+
+
+Outputs:
+-------
+
+"""
