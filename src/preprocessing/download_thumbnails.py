27dc28db97bbc1453c8640c1230e8bae895a72e3
Emily Castelan <emilycastelan@Emilys-Air.hsd1.ca.comcast.net>
2026-03-30 00:56:11 -0400

    reqs.txt updated, new .py started for images phase 2, and folder for image data

---
+++ src/preprocessing/download_thumbnails.py
@@ -0,0 +1,19 @@
+""" 
+root/src/preprocessing/download_thumbnails.py
+Usage: 
+------------
+
+
+Data Collected:
+---------------
+
+
+Methodology:
+------------
+
+Outputs:
+-------
+data/raw/images/
+data/raw/metadata_with_paths.csv
+
+"""
